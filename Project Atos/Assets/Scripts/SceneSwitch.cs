38f40063358b9431f122b69d4f4ed1b3c38b522d
7052roy <leinenga.r@hotmail.nl>
2015-10-06 10:44:34 +0200

    scene switching

---
+++ Project Atos/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSwitch : MonoBehaviour {
+
+    public Camera camera;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(camera.transform.position, camera.transform.forward);
+
+        if (Input.GetButton("Tap"))
+        {
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("test");
+                string name = hit.collider.gameObject.name;
+                if (name == "Screen")
+                {
+                    Application.LoadLevel("liveFeed");
+                }
+
+                // Do something with the object that was hit by the raycast.
+            }
+        }
+    }
+
+}
